849257bea6725823d73124840bf6d3a7e7e65ac7
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-08 02:06:57 +0300

    urls update

--- stock_management_system/stock_management_system/urls.py
+++ stock_management_system/stock_management_system/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('list_item/', views.list_item, name='list_item'),
     path('add_items/', views.add_items, name='add_items'),
     path('update_items/<str:pk>/', views.update_items, name="update_items"),
+    path('delete_items/<str:pk>/', views.delete_items, name="delete_items"),
 ]
