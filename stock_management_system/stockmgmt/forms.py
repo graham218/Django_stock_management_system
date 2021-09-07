249ae29467a63daa0ae45d8d002aea31541cccde
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-08 01:04:44 +0300

    form validation

--- stock_management_system/stockmgmt/forms.py
+++ stock_management_system/stockmgmt/forms.py
@@ -2,9 +2,22 @@ from django import forms
 from .models import *
 
 class StockCreateForm(forms.ModelForm):
-   class Meta:
-     model = Stock
-     fields = ['category', 'item_name', 'quantity']
+	class Meta:
+		model = Stock
+		fields = ['category', 'item_name', 'quantity']
+
+	def clean_category(self):
+		category = self.cleaned_data.get('category')
+		if not category:
+			raise forms.ValidationError('This field is required')
+		return category
+
+
+	def clean_item_name(self):
+		item_name = self.cleaned_data.get('item_name')
+		if not item_name:
+			raise forms.ValidationError('This field is required')
+		return item_name
 
 class StockSearchForm(forms.ModelForm):
    class Meta:
