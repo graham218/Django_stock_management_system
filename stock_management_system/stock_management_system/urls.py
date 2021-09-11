09c047e7433dc7afcad3b312c3b148c64dc83cc2
BILL GRAHAM <69996332+graham218@users.noreply.github.com>
2021-09-11 03:39:57 +0300

    list history url and link on navbar.html

--- stock_management_system/stock_management_system/urls.py
+++ stock_management_system/stock_management_system/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),
     path('reorder_level/<str:pk>/', views.reorder_level, name="reorder_level"),
     path('accounts/', include('registration.backends.default.urls')),
+    path('list_history/', views.list_history, name='list_history'),
 ]
